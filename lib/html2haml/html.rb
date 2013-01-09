e37c8fbfa997dedc54b2a627269bc08c0e3c26b9
Stefan Natchev <stefan.natchev@gmail.com>
2013-01-09 18:35:45 -0500

    escape square brackets in regex

--- lib/html2haml/html.rb
+++ lib/html2haml/html.rb
@@ -197,7 +197,7 @@ module Haml
       def content_without_cdata_tokens
         content.
           gsub(/^\s*<!\[CDATA\[\n/,"").
-          gsub(/^\s*]]>\n/, "")
+          gsub(/^\s*\]\]>\n/, "")
       end
     end
 
