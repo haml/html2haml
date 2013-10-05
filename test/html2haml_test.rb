38e3591b7c8797d761d1b54bb2df179f68abc71e
Roman Kolesnev <rvkolesnev@gmail.com>
2013-10-05 23:35:20 +0400

    Add test for 'succeed' with br

--- test/html2haml_test.rb
+++ test/html2haml_test.rb
@@ -406,4 +406,14 @@ HTML
     assert_equal('%div{:attr => "a & b"}', render('<div attr="a & b"></div>'))
   end
 
+  def test_succeed_with_br_inside_tag
+    assert_equal(<<HAML.rstrip, render('<h5>Your Credit Card will be charged <br>$1.00 for your next month</h5>'))
+%h5
+  Your Credit Card will be charged
+  = succeed "$1.00" do
+    %br/
+  for your next month
+HAML
+  end
+
 end
